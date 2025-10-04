91f06de58b0fb7ee17642e3d67bace81735369dc
jay1535 <habbujayanth@gmail.com>
2025-10-04 19:22:56 +0530

    Created nextjs Project

---
+++ ai-fusion/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="bg-blue-200 p-10 h-50 w-full text-black text-2xl">
+   Hello This is the initial setup of AI Fusion
+   </div>
+  );
+}
